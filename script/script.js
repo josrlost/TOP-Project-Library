b14887bc9b83cbf7a31c91a046aaf956807cbe81
josrlost <197001265+josrlost@users.noreply.github.com.>
2026-08-11 23:32:57 -0500

    Added a basic html table with four basic table heading nested elements, that indicate the title, author, language and number of pages of each book listed. Added basic css styling to differentiate the table like a background color and a basic solid border to the table. Added DOM manipulation to the function displayFromMyLibrary, the function that is supposed to display the each book object instance information. Said function has a for of loop that creates 5 new variables that contain a DOM table row, and some table data to represent each detail info of the book. Then each table data element gets appended into the row, and likewise the row gets appended into the variable that contains a reference to the original table written in the html file

--- script/script.js
+++ script/script.js
@@ -1,25 +1,42 @@
+const bookTable = document.querySelector(".bookTable");
+
+
 const myLibrary = [];
 
-function Book(title, author, pages, language, uid) {
+function Book(title, author, pages, language, hasRead, uid) {
     this.title = title,
     this.author = author,
     this.pages = pages,
     this.language = language,
+    this.hasRead = hasRead;
     this.uid = crypto.randomUUID();
 }
 
-function addToMyLibrary(title, author, pages, language) {
-    let book = new Book(title, author, pages, language);
+function addToMyLibrary(title, author, pages, language, hasRead) {
+    let book = new Book(title, author, pages, language, hasRead);
     myLibrary.push(book);
 }
 
-addToMyLibrary("The Silmarillion", "J. R. R. Tolkien", "480 pages", "EN`");
-addToMyLibrary("2666", "Roberto Bolaño", "1136 pages", "ES");
-addToMyLibrary("Paradiso", "Jose Lezama Lima", "466 pages", "ES")
+addToMyLibrary("The Silmarillion", "J. R. R. Tolkien", "480 pages", "EN", true);
+addToMyLibrary("2666", "Roberto Bolaño", "1136 pages", "ES", false);
+addToMyLibrary("Paradiso", "Jose Lezama Lima", "466 pages", "ES", true);
 
 function displayFromMyLibrary(arr) {
     for(const ele of arr) {
-        console.table(ele);
+        let bookRow = document.createElement("tr");
+        let bookTitle = document.createElement("td")
+        let bookAuthor = document.createElement("td");
+        let bookLang = document.createElement("td");
+        let bookPages = document.createElement("td");
+        bookTitle.textContent = ele.title;
+        bookAuthor.textContent = ele.author;
+        bookLang.textContent = ele.language;
+        bookPages.textContent = ele.pages;
+        bookRow.appendChild(bookTitle);
+        bookRow.appendChild(bookAuthor);
+        bookRow.appendChild(bookLang);
+        bookRow.appendChild(bookPages);
+        bookTable.appendChild(bookRow);
     } 
 }
 
